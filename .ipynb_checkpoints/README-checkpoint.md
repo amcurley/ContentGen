de174266bac1f06666c15ba8b7d47bee4a904058
Aidan Curley <aidanculrey1012@gmail.com>
2020-10-06 19:27:47 -0400

    GAN photo flow done

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -1,6 +1,4 @@
-# **Project Fake Influncer - MeetAbby**
-Yes her name is Abby
-
+# **Project ContentGen**
 
 ## **Problem Statement:**
 
@@ -9,16 +7,16 @@ The influencer marketing industry is expected to be worth 15 billion dollars by 
 
 ## **Project Layout**
 
-### Creating the Computer Generated Influncer: 
+### Creating the Computer Generated Influncer:
 There will be two parts of this project. The first part will be the computer generated influencer. I will be utilizing [StyleGan2](https://github.com/NVlabs/stylegan2) from NVIDIA. One will be for the face and the other will be for the body of the person. After those are generated I will merge the two together creating a final Instagram post. I can then take this person and put them in [vid2vid](https://github.com/NVIDIA/vid2vid) and create a generated dance much like the one here: [TikTok Video](https://vm.tiktok.com/ZMJD8tqGu/).
-  
+
 
 ### Creating the Tweet Generator:
 Onto the second part of the project. This step will be utilizing a Twitter bot to stream live tweets about a certain topic which will be art. The bot will feed the tweets in GPT-2 and then GPT-2 will be generate a response to that tweet and comment that resonspe back to the original tweet. Since this influencer will be interested in art I hope she can build influencer within this community over time evenually leading up to the ability to create and sell her own art.
 
 Down below is flowchart on how this process will work under the hood. As long as the bot is running the portion of the project should be able to run forever in the background eventually up on a server.
-    
-![](./assets/02_tweet_generator_02.png) 
+
+![](./assets/02_tweet_generator_02.png)
 
 ## **Data**
 
--- pages/gan.py
+++ pages/gan.py
@@ -4,34 +4,62 @@ import os, random
 import base64
 import awesome_streamlit as ast
 
+# AWS
+import io
+import random
+import boto3
+
+
+s3 = boto3.resource(
+    service_name = 's3',
+    region_name = 'us-east-2',
+
+    # DO NOT PUSH TO GITHUB WITHOUT REMOVING THESE FIRST
+
+    # DO NOT PUSH TO GITHUB WITHOUT REMOVING THESE FIRST
+
+
+)
+
+def image_from_s3(bucket, key):
+
+    bucket = s3.Bucket(bucket)
+    image = bucket.Object(key)
+    img_data = image.get().get('Body').read()
+
+    return Image.open(io.BytesIO(img_data))
 
 def gen():
-    folder = r'/Users/aidancurley/Documents/dsir/personal/ContentGen/faces'
+    # folder = r'/Users/aidancurley/Documents/dsir/personal/ContentGen/faces'
+    #
+    # a = random.choice(os.listdir(folder))
+    #
+    # file = folder + "/" + a #File to selected image
+    #
+    # image = Image.open(file)
+    nums = random.randint(1, 4)
+    image = image_from_s3('images-contentgen', f'seed{nums}.png')
 
-    a = random.choice(os.listdir(folder))
-
-    file = folder + "/" + a #File to selected image
-
-    image = Image.open(file)
+    file = s3.Bucket('images-contentgen').download_file(f'seed{nums}.png', f'seed{nums}.png')
 
     st.title('Person Generation')
     st.markdown("Click Generate to Get Your Face!")
     if st.button('Generate'):
         st.image(image, use_column_width=True)
-        st.title('Download Generated Face')
-        st.markdown('Click to Download')
-
-        # Give the user a link to download their image
-        def get_image_download_link(img, file_label='File'):
-            with open(img, 'rb') as f:
-                data = f.read()
-            bin_str = base64.b64encode(data).decode()
-            href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(img)}">Download {file_label}</a>'
-            return href
-        # CLickable link for the downloadable image.
-        st.markdown(get_image_download_link(file, 'Generated Face'), unsafe_allow_html=True)
-    else:
-        pass
+    #     st.title('Download Generated Face')
+    #     st.markdown('Click to Download')
+    #
+    #     # Give the user a link to download their image
+    #     def get_image_download_link(img, file_label='File'):
+    #         with open(img, 'rb') as f:
+    #             data = f.read()
+    #         bin_str = base64.b64encode(data).decode()
+    #         href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(img)}">Download {file_label}</a>'
+    #         return href
+    #     # CLickable link for the downloadable image.
+    #     st.markdown(get_image_download_link(image, 'Generated Face'), unsafe_allow_html=True)
+    # else:
+    #     pass
 
 def write():
     """Method used to bring page into the app.py file"""
